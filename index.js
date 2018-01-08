739ea3ca063233ece4ee5f89267248bc8607beba
TuanLM <tuan.le@fujitechjsc.com>
2018-01-08 21:54:36 +0700

    list_view

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 import { Navigation } from 'react-native-navigation';
-import { AppRegistry } from 'react-native';
-import HomeView from './src/components/screens/HomeView.js'
-import DetailView from './src/components/screens/DetailView.js'
-import Routers from './src/routers/Routers.js'
-
-import Store from './src/store/Store.js';
 import { Provider } from 'react-redux';
 
-Routers(Store, Provider)
+import Routers from './src/routers/Routers.js'
+import Store from './src/store/Store.js';
 
-//AppRegistry.registerComponent('demo_react_native', () => HomeView);
+Routers(Store, Provider)
 
 Navigation.startSingleScreenApp({
   screen: {
     screen: 'HomeView',
-    //screen: 'UserDetailViewContainer',
     title: 'HomeView',
     navigatorStyle: {},
     navigatorButtons: {}
--- src/components/screens/HomeView.js
+++ src/components/screens/HomeView.js
@@ -3,16 +3,9 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-import { AppRegistry } from 'react-native';
-import { Button } from 'react-native';
 import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-import { connect } from 'react-redux';
-
-import Button1 from '../views/Button1'
-import Button2 from '../views/Button2'
-import CounterViewContainer from '../../containers/CounterViewContainer.js';
-import Store from '../../store/Store.js';
+import { SearchBar } from 'react-native-elements'
+import { List, ListItem } from 'react-native-elements'
 
 import {
   Platform,
@@ -21,76 +14,42 @@ import {
   View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const list = [
+  {
+    name: 'Amy Farha',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'Vice President'
+  },
+  {
+    name: 'Chris Jackson',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+    subtitle: 'Vice Chairman'
+  },
+]
 
 class HomeView extends Component<{}> {
-  
-  state = {value: 10}
-
-  buttonPress2 = () => {
-    console.log("press")
-    
-    this.props.navigator.push({
-      screen: 'UserDetailViewContainer',
-      title: "UserDetail",
-    });
-  }
-
-  buttonPress1 = () => {
-    console.log("hello")
-    
-    this.setState({value: (this.state.value + 1)}) 
-  }
- 
-  buttonPress = () => {
-    console.log("press")
-    
-    this.props.navigator.push({
-      screen: 'DetailView',
-      title: "Detail",
-    });
-  }
  
  render() {
     return (
-      <Provider store={Store}>
-      <View style={styles.container}>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-        
-        <Button1 title="Button 1 props" />
-        <Button2 title="Button 2 props" />
-        <Button2 />
-
-        <Text>Current State: {this.state.value}</Text>
-
-        <Button
-          onPress={this.buttonPress}
-          title="Push Detail Component"
-          color="#841584"
-        />
-
-        <Button
-          onPress={this.buttonPress2}
-          title="Push User Detail Component"
-          color="#841584"
-        />
-
-        <Button
-          onPress={this.buttonPress1}
-          title="Change State"
-          color="#841584"
-        />
-        
-        <CounterViewContainer/>
+      <View>
+        <SearchBar
+          round
+          lightTheme
+          placeholder='Type Here...' />
+          
+        <List containerStyle={{marginBottom: 20}}>
+          {
+            list.map((l, i) => (
+              <ListItem
+                roundAvatar
+                avatar={{uri:l.avatar_url}}
+                key={i}
+                title={l.name}
+              />
+            ))
+          }
+        </List>
       </View>
-      </Provider>
     );
   }
 }
